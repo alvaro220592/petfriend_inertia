552b4701e9287451fb8660834faa3ac6447ed92c
alvaro220592 <alvaro220592@gmail.com>
2023-01-19 01:18:01 -0300

    Instalação sweetalert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,10 +12,17 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {fas} from "@fortawesome/free-solid-svg-icons";
-
 library.add(fas);
 
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const options = {
+    confirmButtonColor: '#41b882',
+    cancelButtonColor: '#ff7674',
+  };
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -25,6 +32,7 @@ createInertiaApp({
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .use(LaravelPermissionToVueJS)
+            .use(VueSweetalert2, options)
             .component("font-awesome-icon", FontAwesomeIcon)
             .mount(el);
     },
